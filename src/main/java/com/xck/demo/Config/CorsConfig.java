0f5e654441bdf97cdcf7c4a754959e89f94714bf
xiechaokang <508540007@qq.com>
2020-05-14 16:53:13 +0800

    完善部分逻辑，解决跨域访问问题

---
+++ src/main/java/com/xck/demo/Config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.xck.demo.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 谢朝康
+ * @date 2020/5/14 16:07
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
